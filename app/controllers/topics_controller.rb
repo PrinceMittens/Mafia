cb3015a58fed0217d1304a215c44beffe1d87e80
oysong <oysong@ucsc.edu>
2016-07-14 03:02:56 +0000

    fixed topic creation

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,11 +4,12 @@ class TopicsController < ApplicationController
     new_topic = Topic.new
     new_topic.user_id = current_user.id
     new_topic.title = params[:title]
-    if params[:category]
-      new_topic.category = 1
-    else
-      new_topic.category = 0
-    end
+    #if params[:category]
+    #  new_topic.category = 1
+    #else
+    #  new_topic.category = 0
+    #end
+    new_topic.category = params[:category]
     new_topic.content = params[:content]
     new_topic.save
     redirect_to root_path
