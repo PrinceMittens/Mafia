07714ba1b6a2c265c0db2f52d134573b822e1be2
Howard Ting <hting@ucsc.edu>
2016-07-21 01:27:45 +0000

    Sign up correctly stores into table, and search index searches as desired using player.id

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -5,17 +5,22 @@ class Topic < ApplicationRecord
     has_many :Players
   
     # function for searching through the roster by index from latest player
+    # returns -1 if index is nonexistent, meaning no players in game yet
     def player_last_index(index = 0)
-        count = 0
-        tmp_id = self.last_registered_player_id
-        while count < index
-            next_id = Player.find(tmp_id).prev_player_id
-            if next_id == -1
-                break
-            end
-            tmp_id = next_id
-            count += 1
+        temp_id = self.last_registered_player_id
+        if temp_id == -1 || temp_id == nil
+            return -1
+        else
+          count = 0
+          while count < index
+              next_id = Player.find(temp_id).prev_player_id
+              if next_id == -1
+                  break
+              end
+              temp_id = next_id
+              count += 1
+          end
+          return Player.find(temp_id)
         end
-        return Player.find(tmp_id)
     end
 end
